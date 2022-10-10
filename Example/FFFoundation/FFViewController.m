25a034d947f3a5b268b932622195ae8e99090dc7
JiafengLoad <723120415@qq.com>
2022-10-10 23:19:35 +0800

    Initial commit

---
+++ Example/FFFoundation/FFViewController.m
@@ -0,0 +1,29 @@
+//
+//  FFViewController.m
+//  FFFoundation
+//
+//  Created by JiafengLoad on 10/10/2022.
+//  Copyright (c) 2022 JiafengLoad. All rights reserved.
+//
+
+#import "FFViewController.h"
+
+@interface FFViewController ()
+
+@end
+
+@implementation FFViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
