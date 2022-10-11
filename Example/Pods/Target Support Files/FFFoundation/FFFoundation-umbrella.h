6bf5115ef94892537ff1d71522fe50a96c4ed4c0
JiafengLoad <723120415@qq.com>
2022-10-11 22:54:36 +0800

    对示例工程pod install 了下

--- Example/Pods/Target Support Files/FFFoundation/FFFoundation-umbrella.h
+++ Example/Pods/Target Support Files/FFFoundation/FFFoundation-umbrella.h
@@ -10,8 +10,14 @@
 #endif
 #endif
 
+#import "NSString+AppInfo.h"
 #import "NSString+Extension.h"
+#import "NSString+DeviceInfo.h"
+#import "NSArray+Log.h"
+#import "NSDictionary+Log.h"
 #import "NSStringTool.h"
+#import "NSString+Number.h"
+#import "NSString+SystemInfo.h"
 
 FOUNDATION_EXPORT double FFFoundationVersionNumber;
 FOUNDATION_EXPORT const unsigned char FFFoundationVersionString[];
