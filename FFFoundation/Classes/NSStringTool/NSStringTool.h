b46ea02ffb9a3da088763b1d955104cc037c6fca
JiafengLoad <723120415@qq.com>
2022-10-11 22:48:10 +0800

    去掉没用到的头文件

--- FFFoundation/Classes/NSStringTool/NSStringTool.h
+++ FFFoundation/Classes/NSStringTool/NSStringTool.h
@@ -8,16 +8,16 @@
 #import <Foundation/Foundation.h>
 
 #import "NSString+Extension.h"
-#import "NSMutableAttributedStringTool.h"
+//#import "NSMutableAttributedStringTool.h"
 #import "NSString+DeviceInfo.h"
 #import "NSString+SystemInfo.h"
-#import "NSString+EncodeAndDecode.h"
-#import "NSString+DeviceInfo.h"
+//#import "NSString+EncodeAndDecode.h"
+#import "NSString+AppInfo.h"
 
-#import "NSString+Time.h"
+//#import "NSString+Time.h"
 #import "NSArray+Log.h"
 #import "NSDictionary+Log.h"
-#import "NSString+Business.h"
+//#import "NSString+Business.h"
 
 
 
